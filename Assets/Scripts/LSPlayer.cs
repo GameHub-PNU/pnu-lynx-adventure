6e5c95d66253c76384ce7f6f8c49b4439fc07f50
Gryniam <muzikaeng@gmail.com>
2022-05-18 19:11:07 +0300

    Added level scene and move mechanics on this scene

---
+++ Assets/Scripts/LSPlayer.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSPlayer : MonoBehaviour
+{
+
+    public MapPoint currentPoint;
+
+    public float moveSpeed = 10f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentPoint.transform.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, currentPoint.transform.position) < .2f)
+        {
+
+            if (Input.GetAxisRaw("Horizontal") > .5f)
+            {
+                if (currentPoint.right != null)
+                {
+                    SetNextPoint(currentPoint.right);
+                }
+
+            }
+
+            if (Input.GetAxisRaw("Horizontal") < -.5f)
+            {
+                if (currentPoint.left != null)
+                {
+                    SetNextPoint(currentPoint.left);
+                }
+            }
+
+
+            if (Input.GetAxisRaw("Vertical") > .5f)
+            {
+                if (currentPoint.up != null)
+                {
+                    SetNextPoint(currentPoint.up);
+                }
+
+            }
+
+            if (Input.GetAxisRaw("Vertical") < -.5f)
+            {
+                if (currentPoint.down != null)
+                {
+                    SetNextPoint(currentPoint.down);
+                }
+            }
+        }
+    }
+
+
+    public void SetNextPoint(MapPoint nextPoint)
+    {
+        currentPoint = nextPoint;
+    }
+}
---
+++ Assets/Scripts/MapPoint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPoint : MonoBehaviour
+{
+    public MapPoint up, right, down, left;
+    public bool isLevel;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
